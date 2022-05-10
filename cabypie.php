a60031cee0c6406fca70e6496304979fe13c1ce9
Angela Riveros <60806522+Apriveros58@users.noreply.github.com>
2022-05-10 14:26:36 -0500

    Add files via upload

---
+++ cabypie.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>eCommerce para emprendedores</title>
+  </head>
+  <body>
+    <?php
+      function CabeceraPagina(){
+    ?>
+      <h1>Velas Velásquez</h1>
+      <a href="eCommerce.php"><img src="home.png" width="25" height="25"></a>
+      <a href="sesion.php"><img src="sesion.png" width="25" height="25"></a>
+      <a href="verCarrito.php"><img src="carrito.png" width="25" height="25"></a>
+      <br><br>
+      <div class="flexsearch">
+        <div class="flexsearch--wrapper">
+          <form class="flexsearch--form" action="#" method="post">
+            <div class="flexsearch--input-wrapper">
+              <input class="flexsearch--input" type="search" placeholder="Buscar producto">
+              <input class="flexsearch--submit" type="submit" value="&#10140;"/>
+            </div>
+          </form>
+        </div>
+      </div>
+      <br><br>
+    <?php
+      }
+      function PiePagina(){
+    ?>
+      <br><br>
+        <font size="-1">eCommerce para emprendedores Grupo 6<br>
+          Jeisson Ducuara         José Madera         Oswaldo Rincón          Cristian Rodríguez          Vanessa Sandoval          Angela Riveros
+        </font><br>
+          <?php
+      }
+      ?>
+  </body>
+</html>
---
+++ verCarrito.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>eCommerce para emprendedores</title>
+  </head>
+  <body>
+
+      <?php include("cabypie.php") ?>
+      <?php CabeceraPagina(); ?>
+
+      <button>Modificar Lista</button>
+      <img src="vela1.jpg" width="200" height="200">
+      <img src="vela2.jpg" width="200" height="200">
+
+      <br><button>Agregar productos</button>
+      <br><button>Eliminar productos</button>
+      <br><button>Cambiar productos</button>
+      <br><br><br><button>Finalizar Compra</button>
+
+      <?php PiePagina(); ?>
+
+  </body>
+</html>
